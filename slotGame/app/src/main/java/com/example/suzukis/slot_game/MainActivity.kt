2977de2b9efcb16ac189c78578f7b696631e6c9c
pikohan-suzuki <b1702996@planet.kanazawa-it.ac.jp>
2018-05-19 17:51:32 +0900

    リセットボタンの追加
    リセットボタンの追加
    コードの簡略化

--- slotGame/app/src/main/java/com/example/suzukis/slot_game/MainActivity.kt
+++ slotGame/app/src/main/java/com/example/suzukis/slot_game/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.suzukis.slot_game
 import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import java.util.Calendar.MILLISECOND
-
 
 class MainActivity : AppCompatActivity() {
     lateinit var numberImageView1: ImageView
@@ -18,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var stopButton1: Button
     lateinit var stopButton2: Button
     lateinit var stopButton3: Button
-
+    lateinit var resetButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         var slot1: Int = 0
@@ -36,95 +37,78 @@ class MainActivity : AppCompatActivity() {
         numberImageView1 = findViewById(R.id.numberImage1) as ImageView
         numberImageView2 = findViewById(R.id.numberImage2) as ImageView
         numberImageView3 = findViewById(R.id.numberImage3) as ImageView
-
-
-
-
+        resetButton = findViewById(R.id.resetButton) as Button
 
         stopButton1.setOnClickListener() {
-            val calendar = Calendar.getInstance()
-            val ms = calendar.get(Calendar.MILLISECOND)
             slot1Flg =1
-            when (ms % 4) {
-                0 -> {
-                    numberImageView1.setImageResource(R.drawable.doroid2)
-                    slot1 = 0
-                }
-                1 -> {
-                    numberImageView1.setImageResource(R.drawable.number1)
-                    slot1 = 1
-                }
-                2 -> {
-                    numberImageView1.setImageResource(R.drawable.number2)
-                    slot1 = 2
-                }
-                3 -> {
-                    numberImageView1.setImageResource(R.drawable.number3)
-                    slot1 = 3
-                }
-            }
+            slot1=setImage(slot1,numberImageView1,stopButton1)
             if(slot1Flg==1 && slot2Flg==1 && slot3Flg==1){
                 answer(slot1,slot2,slot3)
             }
         }
         stopButton2.setOnClickListener() {
             slot2Flg = 1
-            val calendar = Calendar.getInstance()
-            val ms = calendar.get(Calendar.MILLISECOND)
-            when (ms % 4) {
-                0 -> {
-                    numberImageView2.setImageResource(R.drawable.doroid2)
-                    slot2 = 0
-                }
-                1 -> {
-                    numberImageView2.setImageResource(R.drawable.number1)
-                    slot2 = 1
-                }
-                2 -> {
-                    numberImageView2.setImageResource(R.drawable.number2)
-                    slot2 = 2
-                }
-                3 -> {
-                    numberImageView2.setImageResource(R.drawable.number3)
-                    slot2 = 3
-                }
-            }
+            slot2=setImage(slot2,numberImageView2,stopButton2)
             if(slot1Flg==1 && slot2Flg==1 && slot3Flg==1){
                 answer(slot1,slot2,slot3)
             }
         }
         stopButton3.setOnClickListener() {
             slot3Flg = 1
-            val calendar = Calendar.getInstance()
-            val ms = calendar.get(Calendar.MILLISECOND)
-            when (ms % 4) {
-                0 -> {
-                    numberImageView3.setImageResource(R.drawable.doroid2)
-                    slot3 = 0
-                }
-                1 -> {
-                    numberImageView3.setImageResource(R.drawable.number1)
-                    slot3 = 1
-                }
-                2 -> {
-                    numberImageView3.setImageResource(R.drawable.number2)
-                    slot3 = 2
-                }
-                3 -> {
-                    numberImageView3.setImageResource(R.drawable.number3)
-                    slot3 = 3
-                }
-            }
+            slot3=setImage(slot3,numberImageView3,stopButton3)
             if(slot1Flg==1 && slot2Flg==1 && slot3Flg==1){
                 answer(slot1,slot2,slot3)
             }
         }
+        resetButton.setOnClickListener(){
+            numberImageView1.setImageResource(R.drawable.doroid)
+            numberImageView2.setImageResource(R.drawable.doroid)
+            numberImageView3.setImageResource(R.drawable.doroid)
+            stopButton1.setVisibility(View.VISIBLE)
+            stopButton2.setVisibility(View.VISIBLE)
+            stopButton3.setVisibility(View.VISIBLE)
+            slot1=0
+            slot2=0
+            slot3=0
+            slot1Flg=-1
+            slot2Flg=-1
+            slot3Flg=-1
+            resetButton.setVisibility(View.INVISIBLE)
+        }
     }
+
     fun answer(slot1 :Int,slot2:Int,slot3:Int){
         if(slot1==0 && slot1==slot2 && slot2==slot3){
             Toast.makeText(this,"大当たり！",Toast.LENGTH_LONG).show()
         }else if(slot1==slot2 && slot2==slot3){
             Toast.makeText(this,"当たり！",Toast.LENGTH_LONG).show()
         }
+        resetButton.setVisibility(View.VISIBLE)
+    }
+
+    fun setImage( slot : Int,numberImage : ImageView,stopButton : Button) : Int{
+        val calendar = Calendar.getInstance()
+        val ms = calendar.get(Calendar.MILLISECOND)
+        var slotSub = slot
+        when (ms % 4) {
+            0 -> {
+                numberImage.setImageResource(R.drawable.doroid2)
+                slotSub = 0
+            }
+            1 -> {
+                numberImage.setImageResource(R.drawable.number1)
+                slotSub = 1
+            }
+            2 -> {
+                numberImage.setImageResource(R.drawable.number2)
+                slotSub = 2
+            }
+            3 -> {
+                numberImage.setImageResource(R.drawable.number3)
+                slotSub = 3
+            }
+        }
+        stopButton.setVisibility(View.INVISIBLE)
+        return slotSub
     }
 }
