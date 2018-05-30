24e8502601ff680d5b6d4b21955cd38dbc646c9e
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2018-05-30 23:52:02 +0900

    データベースを追加

---
+++ OmikujiApp/app/src/main/java/com/example/suzukis/omikujiapp/ResultData.kt
@@ -0,0 +1,10 @@
+package com.example.suzukis.omikujiapp
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmClass
+
+@RealmClass
+open class ResultData (@PrimaryKey open var id : Int =0,
+        open var result : String=""
+    ) : RealmObject()
